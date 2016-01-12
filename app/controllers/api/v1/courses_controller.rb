29cf85a089d532c0604439f14117fc173e28a7fa
Jaco Erasmus <jierasmus27@gmail.com>
2016-01-12 22:36:55 +0200

    Correct Course Apipie examples

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::CoursesController < Api::V1::BaseController
 
   api :GET, '/courses', 'List all Courses'
   description "Returns an array of hashes containing all the course data used in the application."
-  this_example = JSON.parse '[{"id": 1,"name": "Coursera MOOC", "description":"Online Ruby Course", "created_at": "2015-10-27T16:08:29.099Z"}]'
+  this_example = JSON.parse '[{"id": 1,"name": "Coursera MOOC"}]'
   example "Response:\n#{JSON.pretty_generate(this_example)}"
 
   def index
@@ -26,7 +26,7 @@ class Api::V1::CoursesController < Api::V1::BaseController
   error 404, "Record Not Found"
   description "Returns a hash with course details based on the id passed through"
   example "Request:\ncourses/1"
-  this_example = JSON.parse '{"id": 1,"name": "Coursera MOOC", "description":"Online Ruby Course", "created_at": "2015-10-27T16:08:29.099Z"}'
+  this_example = JSON.parse '{"id": 1,"name": "Coursera MOOC", "description":"Online Ruby Course", "image_url" : "Url where Image is located", "institution_id" : "1", "instructor_id" : "2", "created_at": "2015-10-27T16:08:29.099Z"}'
   example "Response:\n#{JSON.pretty_generate(this_example)}"
 
   def show
@@ -37,7 +37,7 @@ class Api::V1::CoursesController < Api::V1::BaseController
   description "Creates an course with the given params"
   req_example = JSON.parse '{"name" : "Coursera","description":"Online Course"}'
   example "Request\ncourses/\n#{JSON.pretty_generate(req_example)}"
-  this_example = JSON.parse '{"name": "Coursera MOOC", "description":"Online Ruby Course", "created_at": "2015-10-27T16:08:29.099Z"}'
+  this_example = JSON.parse '{"name": "Coursera MOOC", "description":"Online Ruby Course", "image_url" : "Url where Image is located", "institution_id" : "1", "instructor_id" : "2"}'
   example "Response:\n#{JSON.pretty_generate(this_example)}"
 
   def create
@@ -47,7 +47,7 @@ class Api::V1::CoursesController < Api::V1::BaseController
   api :PATCH, '/courses/:id', 'Update a course'
   error :code => 404, :desc => "Not Found"
   description "Updates an course entry based on the id and params passed"
-  req_example = JSON.parse '{"name": "New Course Name"}'
+  req_example = JSON.parse '{"name": "Coursera MOOC", "description":"Online Ruby Course", "image_url" : "Url where Image is located", "institution_id" : "1", "instructor_id" : "2", "created_at": "2015-10-27T16:08:29.099Z"}'
   example "Request\ncourses/\n#{JSON.pretty_generate(req_example)}"
   this_example = JSON.parse '{"name": "Coursera MOOC", "description":"Online Ruby Course", "created_at": "2015-10-27T16:08:29.099Z"}'
   example "Response:\n#{JSON.pretty_generate(this_example)}"
