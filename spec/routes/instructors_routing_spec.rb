fe3039a7ad629ffe27766c21e010baac42e72d3b
Jaco Erasmus <jierasmus27@gmail.com>
2016-01-16 23:01:10 +0200

    Correct instructor test spec string

--- spec/routes/instructors_routing_spec.rb
+++ spec/routes/instructors_routing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Api::V1::InstructorsController, type: :routing do
   context "routing" do
     it "should route get requests to the index method" do
-      expect(:get => "/rails_resume/api/instructors/").to route_to("api/v1/instructors#index", :format => :json)
+      expect(:get => "/rails_resume/api/instructors").to route_to("api/v1/instructors#index", :format => :json)
     end
 
     it "should route get requests to the show method" do
