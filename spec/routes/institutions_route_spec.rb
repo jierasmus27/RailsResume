6c92433c6c66604f680685e7a80677f7a1039bde
Jaco Erasmus <jierasmus27@gmail.com>
2016-01-16 22:59:30 +0200

    Correct Institutions test string

--- spec/routes/institutions_route_spec.rb
+++ spec/routes/institutions_route_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Api::V1::InstitutionsController, :type => :routing do
   context "routing" do
     it "should route get requests to the index method" do
-      expect(:get => "/rails_resume/api/institutions/").to route_to("api/v1/institutions#index", :format => :json)
+      expect(:get => "/rails_resume/api/institutions").to route_to("api/v1/institutions#index", :format => :json)
     end
 
     it "should route get requests to the show method" do
